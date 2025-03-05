b72e5e362fc0bb767280bf727ff13067cca6fcfe
Ritchmond Tajarros <tajarrosritchmond2002@gmail.com>
2025-03-06 03:36:58 +0800

    first commit

---
+++ data/cart.js
@@ -0,0 +1,29 @@
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 2,
+  },
+];
+
+export function addCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity++;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
